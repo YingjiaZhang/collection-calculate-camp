ff84e39ab7df6fce9a7e99c6e40515a37e1a384f
zhyingjia <965330394@qq.com>
2016-07-26 04:24:36 +0800

    filter_3 commit

--- practices/filter/choose_no_repeat_number.js
+++ practices/filter/choose_no_repeat_number.js
@@ -1,8 +1,20 @@
 'use strict';
 
 function choose_no_repeat_number(collection) {
+  let temp = [];
+  return collection.filter(function (num_a) {
+    let exist = temp.find(function (num) {
+      return num === num_a;
+    });
+    if (exist) {
+      return false;
+    } else {
+      temp.push(num_a);
+      return true;
+    }
+  });
 
-  //在这里写入代码
+
 }
 
 module.exports = choose_no_repeat_number;
