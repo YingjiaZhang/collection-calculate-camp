256b04b4e785f88bc0246283b9557cdd9bc28b13
zhyingjia <965330394@qq.com>
2016-07-26 06:23:31 +0800

    filter_4 commit

--- practices/filter/grouping_count.js
+++ practices/filter/grouping_count.js
@@ -1,8 +1,25 @@
 'use strict';
 
 function grouping_count(collection) {
+  let result = [];
+  collection.filter(function (num_a) {
+    let exist = result.find(function (num_b) {
+      return num_b.key === num_a;
+    });
+    if (exist) {
+      exist.count++;
+    } else {
+      result.push({key: num_a, count: 1});
+    }
+  });
 
-  //在这里写入代码
+
+  let temp = {};
+  result.forEach(function (tmp) {
+    temp[tmp.key] = tmp.count;
+  });
+  return temp;
+
 }
 
 module.exports = grouping_count;
