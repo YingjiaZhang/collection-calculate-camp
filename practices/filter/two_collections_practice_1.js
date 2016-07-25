382946e749539816d4c89077804e01e4087d73b5
zhyingjia <965330394@qq.com>
2016-07-26 06:36:02 +0800

    filter_5 commit

--- practices/filter/two_collections_practice_1.js
+++ practices/filter/two_collections_practice_1.js
@@ -1,8 +1,16 @@
 'use strict';
 
 function choose_common_elements(collection_a, collection_b) {
-
-  //在这里写入代码
+  return collection_a.filter(function (num_a) {
+    let exist = collection_b.find(function (num_b) {
+      return num_a === num_b;
+    });
+    if (exist) {
+      return true;
+    }else{
+      return false;
+    }
+  });
 }
 
 module.exports = choose_common_elements;
