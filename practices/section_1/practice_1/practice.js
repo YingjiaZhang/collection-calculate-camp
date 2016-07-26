913c7021f7802b4e8e7022998a18df50d1a5ecd4
zhyingjia <965330394@qq.com>
2016-07-26 22:49:48 +0800

    section_1_protice_1 commit

--- practices/section_1/practice_1/practice.js
+++ practices/section_1/practice_1/practice.js
@@ -1,5 +1,16 @@
+'use strict';
 function collect_same_elements(collection_a, collection_b) {
-  //在这里写入代码
+// 10:00  10:41 10:48
+  return collection_a.filter(function (num_a) {
+    let exist = collection_b.find(function (num_b) {
+      return num_b === num_a;
+    });
+    if(exist){
+      return true;
+    }else{
+      return false;
+    }
+  });
 }
 
 module.exports = collect_same_elements;
