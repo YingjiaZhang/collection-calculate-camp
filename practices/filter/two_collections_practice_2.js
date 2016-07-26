ef647f34641a90084cf88ee92daa38729fa2026c
zhyingjia <965330394@qq.com>
2016-07-26 08:27:53 +0800

    filter_6 commit

--- practices/filter/two_collections_practice_2.js
+++ practices/filter/two_collections_practice_2.js
@@ -1,8 +1,26 @@
 'use strict';
 
 function choose_no_common_elements(collection_a, collection_b) {
+  let result = [];
+  collection_a.filter(function (num_a) {
+    let exist = collection_b.find(function (num_b) {
+      return num_b === num_a;
+    });
+    if (!exist) {
+      result.push(num_a);
+    }
+  });
+  collection_b.filter(function (num_b) {
+    let exist = collection_a.find(function (num_a) {
+      return num_a === num_b;
+    });
+    if (!exist) {
+      push(num_b);
+    }
+  });
+  return result;
 
-  //在这里写入代码
+
 }
 
 module.exports = choose_no_common_elements;
