2d45823a9e5b7d44da52fc038a59eb85bc0cb8af
zhyingjia <965330394@qq.com>
2016-07-26 10:42:48 +0800

    map_4 commit

--- practices/map/number_map_to_word_over_26.js
+++ practices/map/number_map_to_word_over_26.js
@@ -1,6 +1,8 @@
 'use strict';
-var number_map_to_word_over_26 = function(collection){
-  return ['a','m','aa','ad','y','aa'];
+var number_map_to_word_over_26 = function (collection) {
+  return collection.map(function (num) {
+    return num <= 26 ? String.fromCharCode(num + 96) : String.fromCharCode(num / 26 + 96) + String.fromCharCode(num % 26 + 96);
+  });
 };
 
 module.exports = number_map_to_word_over_26;
