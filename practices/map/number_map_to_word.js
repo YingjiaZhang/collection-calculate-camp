325421de8938e9d465a8f3d0f3300e7e64ef93da
zhyingjia <965330394@qq.com>
2016-07-26 10:54:07 +0800

    map_5 commit

--- practices/map/number_map_to_word.js
+++ practices/map/number_map_to_word.js
@@ -1,6 +1,8 @@
 'use strict';
-var number_map_to_word = function(collection){
-  return ['a','b','c','d','e'];
+var number_map_to_word = function (collection) {
+  collection.map(function (num) {
+    return String.fromCharCode(num + 96);
+  });
 };
 
 module.exports = number_map_to_word;
