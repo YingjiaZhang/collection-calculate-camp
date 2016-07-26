76a13208b5960805be9d4f5070ff948048eda934
zhyingjia <965330394@qq.com>
2016-07-26 10:35:26 +0800

    map_3 commit

--- practices/map/map_to_three_multiples.js
+++ practices/map/map_to_three_multiples.js
@@ -1,6 +1,9 @@
 'use strict';
-var map_to_three_multiples = function(collections){
-  return [3,9,15,12,27];
+var map_to_three_multiples = function (collections) {
+
+  return collections.map(function (num) {
+    return 3 * num;
+  });
 };
 
 module.exports = map_to_three_multiples;
