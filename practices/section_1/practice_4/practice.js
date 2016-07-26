5a1e6fa2a1ae562d7a2aee9a08302a196194fa1e
zhyingjia <965330394@qq.com>
2016-07-26 23:31:11 +0800

    section_1_protice_4 commit

--- practices/section_1/practice_4/practice.js
+++ practices/section_1/practice_4/practice.js
@@ -1,5 +1,16 @@
+'use strict';
 function collect_same_elements(collection_a, object_b) {
-  //在这里写入代码
+
+  let result = [];
+  collection_a.forEach(function (obj_a) {
+    let exist = object_b.value.find(function (num_b) {
+      return obj_a.key === num_b;
+    });
+    if (exist) {
+      result.push(obj_a.key);
+    }
+  });
+  return result;
 }
 
 module.exports = collect_same_elements;
