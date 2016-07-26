64037ff7a1d1040ff071da389f28116458e55618
zhyingjia <965330394@qq.com>
2016-07-26 23:09:15 +0800

    section_1_protice_2 commit

--- practices/section_1/practice_2/practice.js
+++ practices/section_1/practice_2/practice.js
@@ -1,5 +1,22 @@
+'use strict';
 function collect_same_elements(collection_a, collection_b) {
-  //在这里写入代码
+  let result = [];
+  collection_a.forEach(function (num_a) {
+    collection_b.forEach(function (num_b) {
+      if (num_b.length >= 1) {
+        num_b.forEach(function (num) {
+          if (num === num_a) {
+            result.push(num_a);
+          }
+        });
+      } else {
+        if (num_b === num_a) {
+          result.push(num_a);
+        }
+      }
+    });
+  });
+  return result;
 }
 
 module.exports = collect_same_elements;
