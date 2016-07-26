66494e52c23e5e953c2f566a9a87778f323127a5
zhyingjia <965330394@qq.com>
2016-07-26 10:20:32 +0800

    flaten_2 commit

--- practices/flaten/double_dimensional_array_to_one_2.js
+++ practices/flaten/double_dimensional_array_to_one_2.js
@@ -1,8 +1,29 @@
 'use strict';
 
 function double_to_one(collection) {
+  let one_array = [];
+  collection.forEach(function (arr) {
+    if (arr.length >= 1) {
+      arr.forEach(function (num) {
+        one_array.push(num);
+      });
+    } else {
+      one_array.push(arr);
+    }
+  });
 
-  //在这里写入代码
+  let result = [];
+  one_array.forEach(function (num) {
+    let exist = result.find(function (temp) {
+      return num === temp;
+    });
+    if (!exist) {
+      result.push(num);
+    }
+
+  });
+
+  return result;
 }
 
 module.exports = double_to_one;
